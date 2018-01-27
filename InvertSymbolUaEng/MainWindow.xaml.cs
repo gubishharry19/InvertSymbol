cfd34219970cff275add951d74ee9db889f79aa6
gubishharry19 <ihor.gubish@gmail.com>
2018-01-27 21:19:08 +0200

    Initial

---
+++ InvertSymbolUaEng/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace InvertSymbolUaEng
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal NotifyIcon notifyIcon;
+        private ContextMenu contextMenu;
+        private MenuItem menuItem;
+        public MainWindow()
+        {
+            InitializeComponent();
+            new Present(this);
+            WindowState = (WindowState)FormWindowState.Minimized;
+            ShowInTaskbar = false;
+            notifyIcon = new NotifyIcon(); // Declaration 
+            contextMenu = new ContextMenu();
+            menuItem = new MenuItem();
+            // Initialize contextMenu
+            contextMenu.MenuItems.AddRange(
+                        new MenuItem[] { menuItem });
+            // Initialize menuItem
+            menuItem.Index = 0;
+            menuItem.Text = "Exit";
+            menuItem.Click += new System.EventHandler(menuItem1_Click);
+
+            //Initialize notifyIcon
+            notifyIcon.Visible = true;
+            notifyIcon.ContextMenu = contextMenu;
+            notifyIcon.Icon = new System.Drawing.Icon("../../NotifyIcon.ico"); // Shown Icon 
+        }
+
+        private void menuItem1_Click(object Sender, EventArgs e)
+        {
+            // Close the form, which closes the application.
+            this.Close();
+        }
+
+        private void ShowForm()
+        {
+            base.Show();
+            base.WindowState = 0;
+        }
+
+        public event EventHandler ActionConvert = null;
+        private void Button_Click_Convert(object sender, RoutedEventArgs e)
+        {
+            ActionConvert.Invoke(sender, e);
+        }
+    }
+}
---
+++ InvertSymbolUaEng/Present.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace InvertSymbolUaEng
+{
+    class Present
+    {
+        MainWindow main;
+        Model model;
+        string text;
+        KeyboardHook hook = new KeyboardHook();
+        public Present(MainWindow main)
+        {
+            this.main = main;
+            model = new Model();
+            hook.KeyPressed += new EventHandler<KeyPressedEventArgs>(Main_ActionConvert);
+            hook.RegisterHotKey(ModifierKeys.Control, Keys.M);
+            this.main.ActionConvert += Main_ActionConvert;
+        }
+        #region Функція делегату. Реагує при нажатті на кнопку або при нажатті Cntr+M
+        private void Main_ActionConvert(object sender, EventArgs e)
+        {
+            string inv_text;
+            if (Clipboard.ContainsText())
+            {
+                try
+                {
+                    text = Clipboard.GetText();
+                    inv_text = model.Convert(text, Model.IsEng(text));
+                    Clipboard.SetText(inv_text, TextDataFormat.UnicodeText);
+
+                    main.notifyIcon.Text = "Success";
+                    main.notifyIcon.BalloonTipTitle = "Success";
+                    main.notifyIcon.BalloonTipText = "Мало би запрацювати";
+                }
+                catch (Exception ex)
+                {
+                    main.notifyIcon.Text = "Error";
+                    main.notifyIcon.BalloonTipTitle = "Error";
+                    main.notifyIcon.BalloonTipText = "Текст містить символи на двох різних мовних розкладках.\n" +
+                        "В цій версії програми можна конвертувати текст на Eng- та Ua-розкладках\n" +
+                        "Error:" + ex.Message;
+                }
+                finally
+                {
+                    main.notifyIcon.ShowBalloonTip(1000);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не запрацювало. В буфері збережено не текст.");
+                return;
+            }
+
+        }
+    }
+    #endregion
+}
